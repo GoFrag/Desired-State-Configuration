3cfbb841a159ff28a41ca5392172d924d5d3186d
Jeff Patton <jspatton@ku.edu>
2014-01-06 08:52:41 -0600

    Added two new configurations, basicwebserver and domain controller.

---
+++ configurations/DomainController.ps1
@@ -0,0 +1,47 @@
+﻿Configuration DomainController
+{
+    param
+    (
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$ComputerName
+    )
+    Node $ComputerName
+    {
+        WindowsFeature ADDomainServices
+        {
+            <#
+                The following features are installed along with AD-Domain-Services
+                RSAT
+                RSAT-Role-Tools
+                RSAT-AD-Tools
+                RSAT-AD-PowerShell
+            #>
+            Name = "AD-Domain-Services"
+            Ensure = "Present"
+            }
+        WindowsFeature RSATADDS
+        {
+            Name = "RSAT-ADDS"
+            Ensure = "Present"
+            DependsOn = "[WindowsFeature]ADDomainServices"
+            }
+        WindowsFeature RSATADAdminCenter
+        {
+            Name = "RSAT-AD-AdminCenter"
+            Ensure = "Present"
+            DependsOn = "[WindowsFeature]ADDomainServices"
+            }
+        WindowsFeature RSATADDSTools
+        {
+            Name = "RSAT-ADDS-Tools"
+            Ensure = "Present"
+            DependsOn = "[WindowsFeature]ADDomainServices"
+            }
+        WindowsFeature WindowsServerBackup
+        {
+            Name = "Windows-Server-Backup"
+            Ensure = "Present"
+            }
+        }
+    }
